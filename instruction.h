9cffcba698862254f32736415134699ea48b5a01
Rafael Lawisch <lawisch.nh@gmail.com>
2021-10-17 14:22:47 -0300

    add getters for instruction data

--- instruction.h
+++ instruction.h
@@ -16,11 +16,8 @@ public:
         this->isValid = true;
     }
 
-    virtual void id() = 0;
-    virtual void ex() = 0;
-    virtual void wb() = 0;
-
-    void invalidate() {
+    void invalidate()
+    {
         this->isValid = false;
     }
 
@@ -29,19 +26,36 @@ public:
             this->id();
     }
 
-    void runEX() {
+    void runEX()
+    {
         if (this->isValid)
             this->ex();
     }
 
-    void runWB() {
+    void runWB()
+    {
         if (this->isValid)
             this->wb();
     }
-protected:
+
+    QString getOpcode()
+    {
+        return this->opcode;
+    }
+
+    uint getLineNumber()
+    {
+        return this->lineNumber;
+    }
+
+protected:  
+    virtual void id() = 0;
+    virtual void ex() = 0;
+    virtual void wb() = 0;
+
     QString opcode;
     bool isValid;
-    int lineNumber;
+    uint lineNumber;
     RegisterBank* registerBank;
 };
 
